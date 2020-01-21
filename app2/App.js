8383a86ed546586c431cfcdcb068e67a23f0f36b
DaianeBarizon <daianebarizon22@gmail.com>
2020-01-21 16:04:58 -0300

    Construindo frases aleatórias

--- app2/App.js
+++ app2/App.js
@@ -3,22 +3,34 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
   TouchableOpacity,
   Image,
-  ImageBackground,
+  Alert,
 } from 'react-native';
 
-// const ButtonPress = () => {
-//   alert('teste');
-// }
+const newPhrasesRandom = () => {
+  var numberRandom = Math.random();
+  numberRandom = Math.floor(numberRandom * 5);
+
+  var phrases = Array();
+  phrases[0] = 'Não é a linguagem de programação que define o programador, mas sim sua lógica.';
+  phrases[1] = 'Faça como um programador. Quando tudo está errado e confuso, apague tudo e recomece do zero.';
+  phrases[2] = 'Faça da sua vida mais "Responsivo"';
+  phrases[3] = 'Programador: “É simples, pode ser composto. Se firma no concreto e torna-se, abstrato.”';
+  phrases[4] = 'É certo que um programador saiba unir cabos, melhor quando os corticoides humanos temporais sejam sentidos a dedo.';
+
+  var phraseChosen = phrases[numberRandom];
+  Alert.alert(phraseChosen);
+}
 
 const App = () => {
   return (
     <View style={styles.sectionContainer}>
       <Image source={require('./imagens/logo.png')} />
-      <TouchableOpacity style={styles.sectionButton}>
-        <Text style={styles.sectionButtonDescription}>Nova Frases</Text>
+      <TouchableOpacity
+        onPress={newPhrasesRandom}
+        style={styles.sectionButton}>
+        <Text style={styles.sectionButtonDescription}>Nova Frase</Text>
       </TouchableOpacity>
     </View >
   );
