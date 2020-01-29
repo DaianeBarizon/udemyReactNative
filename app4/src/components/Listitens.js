09bef5a529b32b5059952c7975a7577898b304ba
DaianeBarizon <daianebarizon22@gmail.com>
2020-01-29 16:10:02 -0300

    Trabalhando com axios e criando componente itens

--- app4/src/components/Listitens.js
+++ app4/src/components/Listitens.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import { View } from "react-native";
 
 import Itens from './Itens';
+import axios from 'axios';
 
 export default class Listitens extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { listitens: [] };
+    }
+
+    UNSAFE_componentWillMount() {
+        axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+            .then(response => { this.setState({ listitens: response.data }) })
+            .catch(() => { console.log('Erro ao recuperar os dados') });
+    }
+
     render() {
         return (
             <View>
                 <Itens />
-                <Itens />
-                <Itens />
+                {this.state.listitens.map(function (item) { console.log(item.titulo) })}
             </View>
         );
     }
