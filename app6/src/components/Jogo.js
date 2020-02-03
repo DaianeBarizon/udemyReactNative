b087d774e618b295924ac01b9cd2fc76eab00ece
DaianeBarizon <daianebarizon22@gmail.com>
2020-02-03 11:14:01 -0300

    Implementando lógica proposta pelo Curso da Udemy

--- app6/src/components/Jogo.js
+++ app6/src/components/Jogo.js
@@ -5,18 +5,42 @@ import {
     StyleSheet,
 } from 'react-native';
 
-var randomImages = [
-    require('../images/moeda_cara.png'),
-    require('../images/moeda_coroa.png')
-];
+const cara = require('../images/moeda_cara.png');
+const coroa = require('../images/moeda_coroa.png');
 
 export default class Jogo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { resultado: '' };
+    }
+
+    UNSAFE_componentWillMount() {
+        const numAletorio = Math.floor(Math.random() * 2);
+        let caraOuCoroa = '';
+
+        if (numAletorio === 0) {
+            caraOuCoroa = 'cara';
+        } else {
+            caraOuCoroa = 'coroa';
+        }
+
+        this.setState({ resultado: caraOuCoroa })
+    }
+
     render() {
-        return (
-            <View style={styles.background}>
-                <Image source={randomImages[Math.floor(Math.random() * randomImages.length)]} />
-            </View>
-        );
+        if (this.state.resultado === 'cara') {
+            return (
+                <View style={styles.background}>
+                    <Image source={cara} />
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.background}>
+                    <Image source={coroa} />
+                </View>
+            );
+        }
     }
 };
 
