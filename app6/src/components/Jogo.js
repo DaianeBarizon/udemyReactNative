436eb0e00613c86a8c385054941f3f636e64c728
DaianeBarizon <daianebarizon22@gmail.com>
2020-02-03 10:51:15 -0300

    Implementando o componente Jogo e a lógica do Jogo (Minha Solução)

--- app6/src/components/Jogo.js
+++ app6/src/components/Jogo.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import {
     View,
-    Text,
+    Image,
+    StyleSheet,
 } from 'react-native';
+
+var randomImages = [
+    require('../images/moeda_cara.png'),
+    require('../images/moeda_coroa.png')
+];
 
 export default class Jogo extends Component {
     render() {
         return (
-            <>
-                <View><Text>Hello Interface Jogar!</Text></View>
-            </>
+            <View style={styles.background}>
+                <Image source={randomImages[Math.floor(Math.random() * randomImages.length)]} />
+            </View>
         );
     }
 };
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#61bd8c',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+    },
+});
 
